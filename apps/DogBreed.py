7114f1a9ed7740d0870e4c9fbf5b3026fdd6ae93
marvin_hoxha <mhoxha20@epoka.edu.al>
2022-07-21 18:21:29 +0200

    resnet model train

--- apps/DogBreed.py
+++ apps/DogBreed.py
@@ -17,13 +17,15 @@ class DogBreed(object):
     logging.info("Got request.")
     logging.info(f"X={X}.")
     image = X.next()[0][0]
-    #plt.imshow(image)
-    #plt.show()
+    plt.imshow(image)
+    plt.savefig('test.png')
+    plt.show()
     image = image[None,...]
     probs=self._dog_model.predict(image)
     pred = tf.argmax(probs, axis=1)
     idx_to_class = {value: key for key, value in X.class_indices.items()}
-    label = idx_to_class[pred.numpy()[0]]    
+    label = idx_to_class[pred.numpy()[0]]
+    print(label.split(".")[-1])
     return label.split(".")[-1]
 
 
